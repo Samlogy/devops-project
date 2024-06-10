81f9020d11c078a9db3c09e16fcc40caae67ac77
Samlogy <senanisammy@gmail.com>
2024-06-10 17:00:34 +0100

    fix: client app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,52 +1,86 @@
-import React, { useState, useRef } from 'react';
-import './App.css';
+import { useState, useRef } from "react";
+import "./App.css";
 
 export default function App() {
   const [items, setItems] = useState([]);
-  const textRef = useRef()
+  const [file, setFile] = useState(null);
+  const textRef = useRef();
 
-  const onSingleFile = (e) => {
-    e.preventDefault();
-    console.log("single => ", e.target.files[0]);
-    // push => S3
+  const [files, setFiles] = useState([]);
+
+  const handleFileChange = (e) => {
+    if (e.target.files) {
+      setFile(e.target.files[0]);
+    }
   };
 
-  const onMultipleFile = (e) => {
-    e.preventDefault();
-    console.log("multiple => ", e.target.files);
-    // push => S3
-  };
-
-  const onAddItem = (e) => {
+  const onSubmit = (e) => {
     e.preventDefault();
     const newItem = textRef.current.value;
+    console.log(newItem);
     if (!newItem) {
       setItems([...items, { title: newItem }]);
-      textRef.current.value = '';
+      textRef.current.value = "";
       // push to RDS
+    }
+  };
+
+  const handleFilesChange = (e) => {
+    if (e.target.files) {
+      setFiles(e.target.files);
+    }
+  };
+
+  const onSingleFile = async () => {
+    if (file) {
+      console.log("file => ", file);
+      const formData = new FormData();
+      formData.append("file", file);
+      // api call => post
+    }
+  };
+
+  const onMultipleFile = async () => {
+    if (files) {
+      console.log('files => ', files)
+      const formData = new FormData();
+      [...files].forEach((file) => {
+        formData.append("files", file);
+      });
     }
   };
 
   return (
     <div className="container">
       <h2>Todo App</h2>
-      <form onSubmit={onSingleFile}>
+
+      <div>
         <h3>Single File</h3>
-        <input type="file" />
-        <button type="submit">
-          upload
-        </button>
-      </form>
+        <div>
+          <label htmlFor="file" className="sr-only">
+            Choose a file
+          </label>
+          <input id="file" type="file" onChange={handleFileChange} />
+        </div>
+        <button onClick={onSingleFile}>Upload a file</button>
+      </div>
 
-      <form onSubmit={onMultipleFile}>
+      <div>
         <h3>Multiple Files:</h3>
-        <input type="file" multiple />
-        <button type="submit">
-          upload
-        </button>
-      </form>
+        <div className="input-group">
+          <label htmlFor="file" className="sr-only">
+            Choose files
+          </label>
 
-      <form onSubmit={onAddItem}>
+          <input id="file" type="file" multiple onChange={handleFilesChange} />
+        </div>
+
+        <button onClick={onMultipleFile} className="submit">
+          Upload {files.length > 1 ? "files" : "a file"}
+        </button>
+      </div>
+
+      <form onSubmit={onSubmit}>
         <h3>RDS</h3>
         <input type="text" ref={textRef} />
         <button type="submit">Submit</button>
